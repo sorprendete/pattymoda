f5a6075dac66f02e63b381b9f4c52f9c555ec912
Ronald <r.lancha@unsm.edu.pe>
2025-07-16 16:08:46 -0500

    Delete backend directory

--- backend/src/main/java/com/tienda/repository/ConfiguracionTiendaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tienda.repository;
-
-import com.tienda.entity.ConfiguracionTienda;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfiguracionTiendaRepository extends JpaRepository<ConfiguracionTienda, Long> {
-}
